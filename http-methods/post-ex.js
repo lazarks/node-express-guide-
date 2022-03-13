4f4a5c8e122649f283ad4824883edb6939c9a18f
lazarks <sljivic.lazar037@gmail.com>
2022-03-13 14:36:52 +0100

    post method postman example

--- http-methods/post-ex.js
+++ http-methods/post-ex.js
@@ -21,6 +21,14 @@ app.post("/api/people", (req, res) => {
     res.status(200).json({ success: true, person: name });
 });
 
+app.post("/api/postman/people", (req, res) => {
+    let { name } = req.body;
+    if (!name) {
+        return res.status(400).json({ success: false, msg: "please provide name value" });
+    }
+    res.status(201).json({ success: true, data: [...people, name] });
+});
+
 app.post("/login", (req, res) => {
     let { name } = req.body;
     if (name) {
